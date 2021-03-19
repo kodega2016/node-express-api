f42efbe5f71771cf01687b146fc7f860b54402e5
Khadga Bahadur Shrestha <khadgalovecoding2016@gmail.com>
2021-03-19 12:21:04 +0545

    Finish module

--- config/db.js
+++ config/db.js
@@ -2,16 +2,11 @@ const mongoose = require("mongoose");
 
 const connectDb = async () => {
   try {
-    let conn = await mongoose.connect(
-      process.env.NODE_ENV === "development"
-        ? process.env.MONGO_URI_DEV
-        : process.env.MONGO_URI_PROD,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-      }
-    );
+    let conn = await mongoose.connect(process.env.MONGO_URI_PROD, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
     console.log(`Database connection is made ${conn.connection.name}`);
   } catch (e) {
     console.log("Failed to connect to DB");
